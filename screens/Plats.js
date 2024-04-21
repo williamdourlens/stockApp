720f31fffe8143bf1a5b86c5914cde19f69125ef
Quentout187 <quentin.caron@ecoles-epsi.net>
2024-04-21 17:31:09 +0200

    fix bug

--- screens/Plats.js
+++ screens/Plats.js
@@ -23,6 +23,15 @@ const Plats = ({ navigation }) => {
 	return (
 		<ScrollView style={styles.container}>
 			<Text style={styles.Title}>Liste des Plats</Text>
+			<View style={styles.div} >
+						<Text style={styles.nameplat}>Nom</Text>
+						<Text style={[styles.infoplat, styles.quantite]} >qte</Text>
+						<Text style={[styles.infoplat, styles.prix]}>prix</Text>
+
+
+			
+
+					</View>
 			{plats ? (
 				plats.map((item) => (
 					<View style={styles.div} key={item.id_categorie}>
